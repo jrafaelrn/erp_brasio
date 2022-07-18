3792f26c304952520c89c28c1ba509708d46f53f
J Rafael R Nascimento <jose.rafael@usp.br>
2022-07-18 13:42:42 -0300

    Add function to handle and send messagens from listener

--- src/bot/listener.py
+++ src/bot/listener.py
@@ -1,5 +1,6 @@
-import time
-import socket
+from ast import ImportFrom
+import time, socket, telegram
+
 
 HOST = '127.0.0.1'
 PORT = 47221
@@ -23,10 +24,42 @@ def start_listener():
                 data = conn.recv(1024)
                 if not data:
                     break
-                print(data.decode('utf-8'))
-                time.sleep(3)
-                conn.sendall(data)
+                data_decoded = data.decode('utf-8')
+                print(data_decoded)
+                time.sleep(1)
+
+                if handle_message(data_decoded):
+                    conn.sendall('OK')
+                else:
+                    conn.sendall('ERROR')
 
             print('Connection closed.')
 
-    
+
+
+
+def handle_message(data):
+
+    print(f'Handling message: {data}')
+
+    msg_type = data['message_type']
+    content = data['content']
+    chat_id = data['chat_id']
+
+
+    if msg_type == 'text':
+        return send_message(content, chat_id)
+
+
+
+
+def send_message(content, chat_id):
+
+    try:
+        telegram_bot = telegram()
+        telegram_bot.sendMessage(content, chat_id)
+        return True
+    except Exception as e:
+        print(f'Error sending message: {e}')
+        return False
+
