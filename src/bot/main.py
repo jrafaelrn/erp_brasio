eab3f2d4e55909a67da428317eb3cfb5dd4a52bb
J Rafael R Nascimento <jose.rafael@usp.br>
2022-07-19 09:57:33 -0300

    Remove listener and insert env on CD Functions

--- src/bot/main.py
+++ src/bot/main.py
@@ -1,8 +1,5 @@
 from threading import Thread
 import bot
-import listener
-
 
 if __name__ == '__main__':
     Thread(target=bot.start_bot).start()
-    Thread(target=listener.start_listener).start()
