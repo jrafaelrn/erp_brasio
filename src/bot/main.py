610e2dd0c3e8e3675b56d381c2ba248613e9d20e
J Rafael R Nascimento <jose.rafael@usp.br>
2022-07-20 17:58:26 -0300

    Add listen on port 8080

--- src/bot/main.py
+++ src/bot/main.py
@@ -1,5 +1,19 @@
 from threading import Thread
-import bot
+from socket import *
+import bot, os, time
+
+
+def listen():
+    
+    s = socket(AF_INET, SOCK_STREAM)
+    s.bind(('0.0.0.0', 8080))
+    s.listen(1)
+    
+    while True:
+        print('\nWaiting for connection...')
+        time.sleep(10)
+
 
 if __name__ == '__main__':
     Thread(target=bot.start_bot).start()
+    Thread(target=listen).start()
