0c21d60f11506008f91b05183f18bcfedf2386be
J Rafael R Nascimento <jose.rafael@usp.br>
2022-07-21 12:01:55 -0300

    Finished principal implementation

--- src/functions/bank_auto_classification/main.py
+++ src/functions/bank_auto_classification/main.py
@@ -2,30 +2,64 @@ import requests, json, gspread, os
 import pandas as pd
 
 
-def open_bd_bank(self):
+def open_bd_bank():
 
     service_account = os.environ.get('GOOGLE_SERVICE_ACCOUNT_KEY')
-    service_account_json = json.dumps(service_account)
-
-    sa = gspread.service_account(filename=service_account_json)
-    self.bd = sa.open("bd_bot")
-    bd_sheet = self.bd.worksheet('bank')
+    sa = gspread.service_account_from_dict(service_account)
+    bd = sa.open("bd_bot")
+    bd_sheet = bd.worksheet('bank')
     bd_pd = pd.DataFrame(bd_sheet.get_all_records())
     return bd_pd
 
 
+def get_classifications(description):
+
+        bd_bank = open_bd_bank()
+        classes = []
+
+        for row in bd_bank.iterrows():
+
+            original_description = row[1]['DESCRICAO_ORIGINAL']
+            original_description = original_description.strip().replace('\n', ' ').replace('\r', ' ')
+
+            if original_description == description:
+
+                category_erp = row[1]['CATEGORY_ERP']
+                entity_erp = row[1]['ENTITY_ERP']
+                id = f'{category_erp}|{entity_erp}'
+
+                if category_erp == '' or entity_erp == '':
+                    continue
+
+                option = {'category': category_erp, 'entity': entity_erp, 'id': id}
+                classes.append(option)
+        
+        
+        if len(classes) == 0:
+            return '{}'
+
+        df = pd.DataFrame(classes)
+
+        #Remove duplicates
+        df = df.drop_duplicates(subset=['id'], keep='first')
+        df_json = df.to_json(orient='records', force_ascii=False)
+        
+        print(df_json)
+
+        return df_json
 
 
-# Call from Cloud Functions
+#################################
+#   Call from Cloud Functions   #
+#################################
 
 def check(request):
 
     request_json = request.get_json(silent=True)
     print(f'Request JSON: {request_json}')
     
-    response = None
-
     description = request_json['description']
-    
+    response = get_classifications(description)
+    print(f'Response: {response}')
 
-
+    return response
