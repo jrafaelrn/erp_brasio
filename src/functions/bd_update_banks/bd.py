c0e395eb4b908385bd1c5f65558bb6b73225ad3c
J Rafael R Nascimento <jose.rafael@usp.br>
2022-07-27 13:53:48 -0300

    Try first implementation from update bd from banks

---
+++ src/functions/bd_update_banks/bd.py
@@ -0,0 +1,38 @@
+import requests, json, os
+
+#################################
+#       GOOGLE FUNCTION         #
+#################################
+
+def insert(DATA, ARGS=None):
+
+    REGION = os.environ.get("REGION")
+    GCP_PROJECT_ID =  os.environ.get("GCP_PROJECT_ID")
+    FUNCTION_NAME = 'function-bd-transaction'
+    ARGS = 'insert'
+
+    data = json.dumps(DATA)
+    headers = {'Content-Type': 'application/json'}
+    link = f'https://{REGION}-{GCP_PROJECT_ID}.cloudfunctions.net/{FUNCTION_NAME}?{ARGS}'
+    
+    resp = requests.post(link, data = data, headers = headers)
+
+    if resp.status_code == 200:
+        print(f'Function {FUNCTION_NAME} executed!')
+        #print(f'Response: {resp.status_code} - Text: {resp.text} - Content: {resp.content}')
+        
+        try:
+            try:
+                response = json.loads(resp.content)
+            except:
+                response = resp.content
+        except:
+            response = resp.text    
+
+        return response
+    
+    else:
+        print(f'!!! Function {FUNCTION_NAME} not executed! - Data: {data} - Status Code: {resp.status_code} - Text: {resp.text}')
+        return None
+
+
