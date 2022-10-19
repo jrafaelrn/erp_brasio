1b521d8edae99a813d2414ebf632107de0660c76
J Rafael R Nascimento <63913814+jrafaelrn@users.noreply.github.com>
2022-10-19 11:48:15 -0300

    Fix import PagBank description

--- src/functions/bd_update_banks/bank_pagbank.py
+++ src/functions/bd_update_banks/bank_pagbank.py
@@ -12,17 +12,17 @@ def import_extrato_pagbank(extrato_file, conta):
 
     for line in extrato_file.iterrows():     
         
-        cod_transacao = line[1][0]
+        cod_transacao = str(line[1][0])
         data = datetime.strptime(line[1][1], '%d/%m/%Y')
-        tipo_old = line[1][2]
-        descricao = f'{line[1][3]} - {line[1][4]}'
-        doc, nome = extract.extract_cpf_cnpj_cliente_fornecedor_from_description(tipo_old)
+        tipo_old = str(line[1][2])
+        descricao = str(f'{line[1][3]}')
+        doc, nome = extract.extract_cpf_cnpj_cliente_fornecedor_from_description(descricao)
         print(f'DOC: {doc} - NOME: {nome}')
 
-        tipo = extract.extract_type(descricao, tipo_old)
+        tipo = str(extract.extract_type(descricao, tipo_old))
         valor = float(line[1][4].replace(',', '.'))
-        conta = conta.upper()
-        descricao = (f'{tipo_old} - {descricao}').upper()
+        conta = str(conta.upper())
+        descricao = str((f'{tipo_old} - {descricao}').upper())
 
         DATA = {}
         DATA['date_trx'] = line[1][1]
