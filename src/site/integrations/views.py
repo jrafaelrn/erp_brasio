ef0553d6e37ace04a6a3b8cdc845eec4ea97ea52
José Rafael R Nascimento <jose.rafael@usp.br>
2022-12-23 21:03:12 -0300

    Login implemented

--- src/site/integrations/views.py
+++ src/site/integrations/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+
+@login_required(login_url="/user/login/")
 def integrations(request):
     return render(request, "integrations.html")
  
--- src/site/user/views.py
+++ src/site/user/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as login_django
 
 def login(request):
-    return render(request, "login.html")
+
+    if request.method == 'GET':
+        return render(request, "login.html")
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')  
+
+        user = authenticate(request, username=username, password=password)   
+
+        if user:
+            login_django(request, user)
+            return redirect('integrations')
+
+        return render(request, "login.html", {"error": "Usuário ou senha inválidos."})
