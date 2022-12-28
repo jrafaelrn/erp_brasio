c74333d93bef8471eb3ddd89719016a4b4baa879
José Rafael R Nascimento <jose.rafael@usp.br>
2022-12-28 15:13:42 -0300

    Resolve merge

--- src/functions/bd_update_banks/bank_pagbank.py
+++ src/functions/bd_update_banks/bank_pagbank.py
@@ -8,17 +8,21 @@ import extract, bd, json, requests, time
 
 def import_extrato_pagbank(extrato_file, conta):
 
-    for line in extrato_file.iterrows():     
+    for line in extrato_file.iterrows():
         
-        cod_transacao = line[1][0]
+        print(f'Importing PagBank file: {extrato_file}')     
+        
+        cod_transacao = str(line[1][0])
         data = datetime.strptime(line[1][1], '%d/%m/%Y')
-        tipo_old = (line[1][2]).upper()
-        descricao = (line[1][3]).upper()
-        doc, nome = extract.extract_cpf_cnpj_cliente_fornecedor_from_description(tipo_old)
-        tipo = extract.extract_type(descricao, tipo_old)
+        tipo_old = str(line[1][2])
+        descricao = str(f'{tipo_old} - {line[1][3]}')
+        doc, nome = extract.extract_cpf_cnpj_cliente_fornecedor_from_description(descricao)
+        print(f'DOC: {doc} - NOME: {nome}')
+
+        tipo = str(extract.extract_type(descricao, tipo_old))
         valor = float(line[1][4].replace(',', '.'))
-        conta = conta.upper()
-        descricao = f'{tipo_old} - {descricao}'
+        conta = str(conta.upper())
+        descricao = descricao.upper()
 
         DATA = {}
         DATA['date_trx'] = line[1][1]
