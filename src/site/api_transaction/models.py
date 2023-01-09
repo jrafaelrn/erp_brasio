26697a4cc4e40ff5ed0cef9f016e385cd775e8aa
jrrn <jose.rafael@usp.br>
2023-01-09 19:06:10 -0300

    Check db hostname and implement models for Ifood

--- src/site/api_transaction/models.py
+++ src/site/api_transaction/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from integrations.models import IntegrationTransaction
 
-# Create your models here.
+
+# Represents a sales transaction
+class Transaction(models.Model):
+    
+    api = models.ForeignKey(IntegrationTransaction, on_delete=models.PROTECT, related_name='api_source')
+    id = models.CharField(max_length=100, primary_key=True)
+    created_at = models.DateTimeField()
+    closed_at = models.DateTimeField()
+    
+    
+
+
+class Ifood(models.Model):
+    
+    transaction = models.ForeignKey(Transaction, on_delete=models.CASCADE, related_name='transaction_ifood')
+    disolay_id = models.CharField(max_length=10)
+    order_type = models.CharField(max_length=20)
+    order_timing = models.CharField(max_length=20)
+    sales_channel = models.CharField(max_length=30)
+    extra_info = models.CharField(max_length=100)
